df02ecae157a8adce968675d36893736f4cc31d8
micky1 <mickytsegaye21@gmail.com>
2017-11-10 14:56:06 -0500

    Mocked Backend
    mocked ingredient backend result , which is to be used to create the
    fronted drop down selections.

--- app/viewSearch/ctrlSearch.js
+++ app/viewSearch/ctrlSearch.js
@@ -5,9 +5,28 @@ angular.module('s2n.viewSearch', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/search', {
     templateUrl: ' viewSearch/viewSearch.html',
-    controller: 'SearchCtrl'
+    controller: 'MockController'
   });
 }])
-    .controller('SearchCtrl', [function() {
+.controller('searchCtrl', [function() {
 
 }]);
+
+var myapp = angular.module('s2n.viewSearch');
+console.log("hey  ");
+myapp.controller('MockController',['$scope','$http',function($s,$h){
+  console.log("here i am");
+  var myctrl = this
+  myctrl.data = {}
+  myctrl.load_data = function(){
+    $h.get("/viewSearch/dummy_ingredient.json").success(function(data){
+      myctrl.data = data;
+      console.log(myctrl.data);
+    }).error(function(){
+      alert("Unable to Load Mock Data");
+    });
+  };
+  myctrl.load_data();
+
+}]);
+
