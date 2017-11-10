01799421c6a00656a92ddf27bc5197bcf25a65fe
RyleySevier <ryleysevier@gmail.com>
2017-11-10 10:19:40 -0500

    Added new template page

---
+++ app/viewTemplate/viewTemplate_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('s2n.viewTemplate module', function() {
+
+  beforeEach(module('sn2.viewTemplate'));
+
+  describe('Template controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var TemplateCtrl = $controller('TemplateCtrl');
+      expect(AboutCtrl).toBeDefined();
+    }));
+
+  });
+});
