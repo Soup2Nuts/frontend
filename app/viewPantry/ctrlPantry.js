f7fe6bf654b0d17eb8164cd163c87bae1dea7951
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2017-11-10 22:44:16 -0500

    Updated View
    + checkbox list of food items
    + pantry autocomplete, but w/o js

--- app/viewPantry/ctrlPantry.js
+++ app/viewPantry/ctrlPantry.js
@@ -8,10 +8,18 @@ angular.module('s2n.viewPantry', ['ngRoute'])
     controller: 'PantryCtrl'
   });
 }])
-    .controller('PantryCtrl', [function() {
+    .controller('PantryCtrl', ['$scope', function($scope) {
+      var self = this
       angular.module('fabSpeedDialDemoBasicUsage', ['ngMaterial'])
       this.topDirections = ['left', 'up'];
       this.bottomDirections = ['down', 'right'];
       this.isOpen = false;
       this.selectedDirection = 'up';
+
+      $scope.foods = [
+       { name: 'Pepperoni', selected: false },
+       { name: 'Sausage', selected: false },
+       { name: 'Black Olives', selected: false },
+       { name: 'Green Peppers', selected: false }
+     ];
 }]);
