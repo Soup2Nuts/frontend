10c4be9bfdd2234cffc68f6a6a0edb3d27b07356
micky1 <mickytsegaye21@gmail.com>
2017-11-11 14:44:20 -0500

    without collapse
    sample render commit , before working on collapse

--- app/viewSearch/ctrlSearch.js
+++ app/viewSearch/ctrlSearch.js
@@ -15,12 +15,15 @@ angular.module('s2n.viewSearch', ['ngRoute'])
 var myapp = angular.module('s2n.viewSearch');
 console.log("hey  ");
 myapp.controller('MockController',['$scope','$http',function($s,$h){
-  console.log("here i am");
+  //console.log("here i am");
   var myctrl = this
   myctrl.data = {}
+  
+  
   myctrl.load_data = function(){
     $h.get("/viewSearch/dummy_ingredient.json").success(function(data){
       myctrl.data = data;
+      $s.ingredients = myctrl.data;
       console.log(myctrl.data);
     }).error(function(){
       alert("Unable to Load Mock Data");
