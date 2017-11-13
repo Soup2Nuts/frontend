3a7cce97934c48dcbaabfd7c370a864df984c164
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2017-11-12 20:16:33 -0500

    Basic setup for the Favorites Page

---
+++ app/viewFavorites/viewFavorites_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('s2n.viewTFavorites module', function() {
+
+  beforeEach(module('sn2.viewFavorites'));
+
+  describe('Favorites controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var FavoritesCtrl = $controller('FavoritesCtrl');
+      expect(FavoritesCtrl).toBeDefined();
+    }));
+
+  });
+});
