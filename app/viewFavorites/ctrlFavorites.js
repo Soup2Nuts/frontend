15451b5a6606f309d3ac8e7c542b46742545bf22
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2017-11-12 23:06:58 -0500

    Added delete favorite functionality

--- app/viewFavorites/ctrlFavorites.js
+++ app/viewFavorites/ctrlFavorites.js
@@ -7,6 +7,188 @@ angular.module('s2n.viewFavorites', ['ngRoute'])
     controller: 'FavoritesCtrl'
   });
 }])
-    .controller('FavoritesCtrl', [function() {
+    .controller('FavoritesCtrl', ['$scope',function($scope) {
+    $scope.recipes = [
+          {
+              "title": "Banana Split Oatmeal",
+              "source": "https://whatscooking.fns.usda.gov/recipes/supplemental-nutrition-assistance-program-snap/banana-split-oatmeal",
+              "cuisines": [],
+              "courses": [
+                  "Breakfast"
+              ],
+              "ingredients": [
+                  {
+                      "quantity": {
+                          "fraction": "1/3",
+                          "unit": "C"
+                      },
+                      "food": "oatmeal",
+                      "notes": "dry quick-cooking"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1/8",
+                          "unit": "tsp"
+                      },
+                      "food": "salt",
+                      "notes": ""
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "3/4",
+                          "unit": "C"
+                      },
+                      "food": "water",
+                      "notes": "very hot"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1/2",
+                          "unit": ""
+                      },
+                      "food": "banana",
+                      "notes": "sliced"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1/2",
+                          "unit": "C"
+                      },
+                      "food": "frozen yogurt",
+                      "notes": "non-fat"
+                  }
+              ]
+          },
+          {
+              "title": "Summer Fruit Salad",
+              "source": "https://whatscooking.fns.usda.gov/recipes/supplemental-nutrition-assistance-program-snap/summer-fruit-salad",
+              "cuisines": [],
+              "courses": [
+                  "Dessert",
+                  "Salad"
+              ],
+              "ingredients": [
+                  {
+                      "quantity": {
+                          "fraction": "1",
+                          "unit": "C"
+                      },
+                      "food": "strawberries",
+                      "notes": "diced fresh or frozen"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1",
+                          "unit": "C"
+                      },
+                      "food": "watermelon",
+                      "notes": "cubed"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1",
+                          "unit": "C"
+                      },
+                      "food": "pineapple chunks",
+                      "notes": "fresh or canned packed in natural juice and do not drain"
+                  }
+              ]
+          },
+          {
+              "title": "Chicken Vegetable Soup with Kale",
+              "source": "https://whatscooking.fns.usda.gov/recipes/supplemental-nutrition-assistance-program-snap/chicken-vegetable-soup-kale",
+              "cuisines": [],
+              "courses": [
+                  "Soup"
+              ],
+              "ingredients": [
+                  {
+                      "quantity": {
+                          "fraction": "2",
+                          "unit": "tsp"
+                      },
+                      "food": "vegetable oil",
+                      "notes": ""
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1/2",
+                          "unit": "C"
+                      },
+                      "food": "onion",
+                      "notes": "chopped"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1/2",
+                          "unit": "C"
+                      },
+                      "food": "carrot",
+                      "notes": "chopped"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1",
+                          "unit": "tsp"
+                      },
+                      "food": "thyme",
+                      "notes": "ground"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "2",
+                          "unit": ""
+                      },
+                      "food": "garlic clove",
+                      "notes": "minced"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "2",
+                          "unit": "C"
+                      },
+                      "food": "water",
+                      "notes": "or chicken broth"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "3/4",
+                          "unit": "C"
+                      },
+                      "food": "tomatoes",
+                      "notes": "diced"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1",
+                          "unit": "C"
+                      },
+                      "food": "chicken",
+                      "notes": "cooked skinned and cubed"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1/2",
+                          "unit": "C"
+                      },
+                      "food": "brown rice",
+                      "notes": "cooked or white rice"
+                  },
+                  {
+                      "quantity": {
+                          "fraction": "1",
+                          "unit": "C"
+                      },
+                      "food": "kale",
+                      "notes": "chopped about one large leaf"
+                  }
+              ]
+          }
+    ];
+
+    $scope.deleteFavorite = function($recipe){
+      var index = $scope.recipes.indexOf($recipe)
+      $scope.recipes.splice(index, 1);
+    }
 
 }]);
